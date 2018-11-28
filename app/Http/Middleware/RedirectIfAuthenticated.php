e6086165d9ff8e063d728b21044fa472fde1d7af
vagrant <vagrant@homestead>
2018-11-28 11:21:03 +0800

    change add spread gener link

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,9 +17,10 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (!Auth::guard($guard)->check()) {
-           
-        }
+        if (Auth::guard('admin')->check()) {
+           return redirect('admin');
+        } 
+
         return $next($request);
     }
 }
